17c124c1c21712d602188711f6552b64b711185e
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-07-06 22:01:57 +0530

    commit

--- JAVA/StacksProblem.java
+++ JAVA/StacksProblem.java
@@ -19,7 +19,7 @@ public class StacksProblem {
     }
     
     public static void main(String[] args) {
-        Stack<Integer> s = new Stack<>();
+        Stack<Integer> s = new Stack<>()
         s.push(1);
         s.push(2);
         s.push(3);
