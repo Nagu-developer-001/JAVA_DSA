7da395beb2bd7e6a9f5dbbb3353a4f52b587aab1
Nagu-developer-001 <nagabhoshanabhat@gmail.com>
2026-09-20 19:26:07 +0530

    Queue

--- JAVA/Classroom.java
+++ JAVA/Classroom.java
@@ -1,8 +1,54 @@
 import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 public class Classroom {
+    static class Queue{
+        java.util.Deque<Integer> dq = new java.util.LinkedList<>();
+        public void add(int data){
+            dq.addLast(data);
+        }
+        public int remove(){
+            return(dq.removeFirst());
+        }
+        public int peek(){
+            return(dq.getFirst());
+        }
+    }
+    static class Stack{
+        Deque<Integer> dq = new LinkedList<>();
+        public void push(int data){
+            dq.addLast(data);
+        }
+        public int pop(){
+            return(dq.removeLast());
+        }
+        public int peek(){
+            return(dq.getLast());
+        }
+    }
+    public static void reverse(java.util.Queue<Integer> q){
+java.util.Stack<Integer> st = new java.util.Stack<>();
+        while(!q.isEmpty()){
+            st.push(q.remove());
+        }
+        while(!st.isEmpty()){
+            q.add(st.pop());
+        }
+    }
+    public static void interleave(java.util.Queue<Integer> q){
+        int size = q.size();
+        java.util.Queue<Integer> fq = new java.util.LinkedList<>();
+        for(int i=0;i<size/2;i++){
+            fq.add(q.remove());
+        }
+        while(!fq.isEmpty()){
+            q.add(fq.remove());
+            q.add(q.remove());
+        }
+    }
     public static void FirstNonRepeatingCharacter(String  str){
         int[] freq = new int[26];
-        Queue<Character> q =new java.util.LinkedList<>();
+        java.util.Queue<Character> q =new java.util.LinkedList<>();
         for(int i=0;i<str.length();i++){
             char ch = str.charAt(i);
             q.add(ch);
@@ -20,7 +66,48 @@ public class Classroom {
         }
     }
     public static void main(String[] args) {
-        String str = "aabccxb";
-        FirstNonRepeatingCharacter(str);
-}
+        //String str = "aabccxb";
+        //FirstNonRepeatingCharacter(str);
+        //Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // q.add(6);
+        // q.add(7);
+        // q.add(8);
+        // q.add(9);
+        // q.add(10);
+        // interleave(q);
+        // System.out.println(q);
+        // Queue<Integer> q = new LinkedList<>();
+        // q.add(1);
+        // q.add(2);
+        // q.add(3);
+        // q.add(4);
+        // q.add(5);
+        // reverse(q);
+        // System.out.print(" "+q);
+        //Stack s = new Stack();
+        //s.push(1);
+        // s.push(2);
+        // s.push(3);
+        // s.push(4);
+        // s.push(5);
+        // System.out.println(s.peek());
+        // while(!s.dq.isEmpty()){
+        //     System.out.print(s.pop() + " ");
+        // }
+        Queue q = new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        System.out.println(q.peek());
+        while(!q.dq.isEmpty()){
+            System.out.print(q.remove()+ " ");
+        }
+    }
 }
